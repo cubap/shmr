45792b0b120bf4fb01cb3a6087644a67cc293f42
unknown <bhaberbe@CAS-THE-40916N8.ds.slu.edu>
2015-03-02 16:15:50 -0600

    new project files and project set up from Han

---
+++ Source Packages/servlet/SaveNewRangeServlet.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright 2015- Saint Louis University. Licensed under the
+ *	Educational Community License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.osedu.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package servlet;
+
+import common.Constant;
+import entity.Annotation;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author hanyan
+ */
+public class SaveNewRangeServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Annotation anno = new Annotation();
+        anno.setNamespace(request.getParameter("namespace"));
+        anno.setContent(request.getParameter("content"));
+//        anno.setSelector(request.getParameter("selector"));
+        anno.setTitle(request.getParameter("title"));
+//        anno.setResource(URLEncoder.encode(request.getParameter("resource"), "utf-8"));
+//        anno.setResourceType(request.getParameter("resourceType"));
+//        anno.setOutterRelative(request.getParameter("outterRelative"));
+        anno.setAddedTime(System.currentTimeMillis()); //This value is set in annotationStore.
+//        anno.setFontColor(request.getParameter("fontColor"));
+//        anno.setFontType(request.getParameter("fontType"));
+        if(null != request.getParameter("permission")){
+            anno.setPermission(Integer.parseInt(request.getParameter("permission")));
+        }else{
+            anno.setPermission(0);
+        }
+        anno.setOriginalAnnoID("");
+        anno.setVersionNum(1); // this value is set in annotationStore. 
+        anno.setForkFromID(request.getParameter("forkFromID"));
+        try {
+            URL postUrl = new URL(Constant.ANNOTATION_SERVER_ADDR + "/anno/saveNewAnnotation");
+            HttpURLConnection connection = (HttpURLConnection) postUrl.openConnection();
+            connection.setDoOutput(true);
+            connection.setDoInput(true);
+            connection.setRequestMethod("POST");
+            connection.setUseCaches(false);
+            connection.setInstanceFollowRedirects(true);
+            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            connection.connect();
+            DataOutputStream out = new DataOutputStream(connection.getOutputStream());
+            //TODO value to save
+            out.writeBytes(request.getParameter("canvasToSave"));
+            out.flush();
+            out.close(); // flush and close
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream(),"utf-8"));
+            String line="";
+            StringBuilder sb = new StringBuilder();
+            while ((line = reader.readLine()) != null){
+                //line = new String(line.getBytes(), "utf-8");  
+                System.out.println(line);
+                sb.append(line);
+            }
+            reader.close();
+            connection.disconnect();
+            response.setHeader("Content-Location", "absoluteURI");
+            response.getWriter().print(sb.toString());
+        } catch (UnsupportedEncodingException ex) {
+            Logger.getLogger(SaveNewCanvasServlet.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(SaveNewCanvasServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doPost(req, resp);
+    }
+}
